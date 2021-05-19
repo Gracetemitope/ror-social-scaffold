a6c19ef2a5dd1a4ca55dddb083a05bea9c5a4fd3
Ajanaku Temitope Grace <gracetemitope14@gmail.com>
2021-05-18 22:24:23 -0700

    complete unit testing

--- db/migrate/20210519004704_add_inivite.rb
+++ db/migrate/20210519004704_add_inivite.rb
@@ -2,6 +2,7 @@ class AddInivite < ActiveRecord::Migration[5.2]
   def change
     add_column :friendships, :inviter_id, :integer
     add_index :friendships, :inviter_id
+    
     add_column :friendships, :invitee_id, :integer
     add_index :friendships, :invitee_id
   end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -28,14 +28,40 @@ end
   end
 end
 
-  
- 
-  describe 'associations' do
-    before do 
+  describe 'inviter associations' do
+    before do
       @user = User.create(name: 'Ifeoluwa', email: 'ife@email.com', password: 'password', gravatar_url: '')
       @user = User.create(name: 'Abiola', email: 'abigold@email.com', password: 'password', gravatar_url: '')
+    end
+    let(:inviter) { User.find_by_name('Ifeoluwa') }
+    let(:invitee) { User.find_by_name('Abiola') }
+    let(:friendship_request) { inviter.friendships.build(invitee_id: invitee.id) }
 
+    it 'can invite another user to be a friend' do
+      expect(friendship_request).to be_valid
+    end
+
+    it 'can see the name of the invitee' do
+      invitee_user = User.find(friendship_request.invitee_id)
+      invitee_name = User.find(invitee_user.id).name
+      expect(invitee_name).to eq('Abiola')
     end
   end
 
-end
+  describe 'invitee association' do
+    before do
+      @user = User.create(name: 'Ifeoluwa', email: 'ife@email.com', password: 'password', gravatar_url: '')
+      @user = User.create(name: 'Abiola', email: 'abigold@email.com', password: 'password', gravatar_url: '')
+    end
+
+    let(:inviter) { User.find_by_name('Ifeoluwa') }
+    let(:invitee) { User.find_by_name('Abiola') }
+    let(:friendship_request) { inviter.friendships.create(invitee_id: invitee.id) }
+
+    it 'can see the name of the inviter' do
+      inviter_user = User.find(friendship_request.inviter_id)
+      inviter_user_name = User.find(inviter_user.id).name
+      expect(inviter_user_name).to eq('Ifeoluwa')
+    end
+  end
+  end
